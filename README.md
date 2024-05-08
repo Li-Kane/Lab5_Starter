268df796a3bb77c1eeebec2f036f0414b4e9a574
Li-Kane <122249106+Li-Kane@users.noreply.github.com>
2024-05-07 22:28:56 -0700

    jest unit testing

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Lab 5 - Starter
+
+1. I believe that unit testing wouldn't be used in testing a "messaging" feature, since sending a message between different users likely requires testing multiple components, which is no longer a single "unit". 
+2. A unit test would be appropriate for testing "max message length", since that is a singular component, making it simple, and isolated from other components to test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,87 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+// test isPhoneNumber
+test('716-271-8821 should be a phone number', () => {
+  expect(isPhoneNumber('716-271-8821')).toBe(true);
+});
+
+test('(216)-292-8221 should be a phone number', () => {
+  expect(isPhoneNumber('(216)-292-8221')).toBe(true);
+});
+
+test('71-271-8821 should not be a phone number', () => {
+  expect(isPhoneNumber('-8821')).toBe(false);
+});
+
+test('72-7272-wtf should not be a phone number', () => {
+  expect(isPhoneNumber('7272-')).toBe(false);
+});
+
+// test isEmail
+test('you@gmail.com should be an email', () => {
+  expect(isEmail('you@gmail.com')).toBe(true);
+})
+
+test('who@ucsd.edu should be an email', () => {
+  expect(isEmail('who@ucsd.edu')).toBe(true);
+})
+
+test('you@.com should not be an email', () => {
+  expect(isEmail('you@.com')).toBe(false);
+})
+
+test('why@gmail should not be an email', () => {
+  expect(isEmail('why@gmail')).toBe(false);
+})
+
+// test isStrongPassword
+test('strong_password should be a strong password', () => {
+  expect(isStrongPassword('strong_password')).toBe(true);
+})
+
+test('super_secure should be a strong password', () => {
+  expect(isStrongPassword('super_secure')).toBe(true);
+})
+
+test('pa should not be a strong password', () => {
+  expect(isStrongPassword('pa')).toBe(false);
+})
+
+test('123456 should not be a strong password', () => {
+  expect(isStrongPassword('123456')).toBe(false);
+})
+
+// test isDate
+test('1/1/2027 should be a date', () => {
+  expect(isDate('1/1/2027')).toBe(true);
+})
+
+test('12/31/2027 should be a date', () => {
+  expect(isDate('12/31/2027')).toBe(true);
+})
+
+test('322/31/2027 should not be a date', () => {
+  expect(isDate('322/31/2027')).toBe(false);
+})
+
+test('12/31/203 should not be a date', () => {
+  expect(isDate('12/31/203')).toBe(false);
+})
+
+// test isHexColor
+test('#BBB should be a hex color', () => {
+  expect(isHexColor('#BBB')).toBe(true);
+})
+
+test('#BBBCCC should be a hex color', () => {
+  expect(isHexColor('#BBBCCC')).toBe(true);
+})
+
+test('#FFFF should not be a hex color', () => {
+  expect(isHexColor('#FFFF')).toBe(false);
+})
+
+test('#GGG should not be a hex color', () => {
+  expect(isHexColor('#GGG')).toBe(false);
+})
